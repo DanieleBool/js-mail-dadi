49a55f5dfeb0c006f446dfa6bc213b198042cf0b
Daniele Di Eleuterio <daniele.dieleuterio@gmail.com>
2021-11-10 20:44:51 +0100

    esercizio finito

--- Gioco_dei_dadi/script.js
+++ Gioco_dei_dadi/script.js
@@ -1,14 +1,44 @@
-// stampo il numero del giocatore
-let  player = Math.floor(Math.random() * 6) + 1;
-alert('Il tuo numero è ' + player);
-// stampo il numero del computer
-let  pc = Math.floor(Math.random() * 6) + 1;
-alert('Il numero del tuo avversario è ' + pc);
+const open = document.querySelector('.game');
 
-if (player > pc) {
-    alert('VITTORIA!');
-}else if ( player < pc) {
-    alert('HAI PERSO.');
-} else {
-    alert('PAREGGIO');
+open.addEventListener("click", 
+function()
+{
+
+// stampo il numero del giocatore
+let player = Math.floor(Math.random() * 6) + 1;
+alert('Il tuo numero è:<br>' + player);
+// stampo il numero del computer
+let pc = Math.floor(Math.random() * 6) + 1;
+alert('Il numero del tuo avversario è:<br>' + pc);
+
+if(player > pc){
+    document.querySelector('.result').innerHTML = 'VITTORIA!';
+}
+else if( player < pc){
+    document.querySelector('.result').innerHTML = 'HAI PERSO.';
+}
+else{
+    document.querySelector('.result').innerHTML = 'PAREGGIO';
+}
+
+}
+);
+
+var vis = 1000;
+
+//MODIFICA ALERT
+window.alert = function (message) {
+  var a = document.createElement('div');
+//regole di stile CSS
+  a.style.cssText = "width:50vw; height:25vh; border:5px solid gold; border-radius:10px; padding:10px; background:white; position:fixed; top:50px; right:0; left:0; margin:auto; font-family: Arial, sans-serif; text-align: center; line-height: 60px; padding: 2% ; font-size: 32px; font-weight: bold; z-index:"+ vis+ ";";
+
+  a.innerHTML = message;
+  document.body.appendChild(a);
+//Rimozione dell'elemento al click
+    a.addEventListener("click", function() {
+    a.remove();
+    }
+    )
+
+vis--;
 }
